adcb17d722b598181a0347a5b6a934de55776400
“thegeralbeanQD” <giquigeraldine@outlook.com>
2023-05-25 15:35:54 +0100

    generate random hero and get stats for each hero

--- src/Pages/Fight/index.jsx
+++ src/Pages/Fight/index.jsx
@@ -4,6 +4,7 @@ import './Fight.css';
 
 const Fight = () => {
   const [selectedHero, setSelectedHero] = useState(null);
+
   const [image, setImage] = useState('');
   const [data, setData] = useState([]);
 
@@ -20,7 +21,6 @@ const Fight = () => {
     } else {
       setSelectedHero(data.results[0]);
       setImage(data.results[0].image.url);
-      console.log(data.results);
     }
   };
 
@@ -29,9 +29,33 @@ const Fight = () => {
     setImage(hero.image.url);
   };
 
-  const handleStart = () => {
-    console.log('start');
+  const handleStart = async () => {
+    let id = Math.floor(Math.random() * 731) + 1;
+    const response = await fetch(
+      `https://www.superheroapi.com/api.php/6382134991809408/${id}`
+    );
+    const randomHeroData = await response.json();
+    
+    let selectedHeroStats = selectedHero.powerstats
+    let randomHeroStats = randomHeroData.powerstats
+    console.log(selectedHeroStats);
+    console.log(randomHeroStats);
+
+    let totalSelectedHeroPower = 0;
+    let totalRandomHeroPower = 0;
+
+    for (let stat in selectedHeroStats) {
+      totalSelectedHeroPower += parseInt(selectedHeroStats[stat]);
+    }
+
+    for (let stat in randomHeroStats) {
+      totalRandomHeroPower += parseInt(randomHeroStats[stat]);
+    }
+
+    console.log('Total Power of Selected Hero:', totalSelectedHeroPower);
+    console.log('Total Power of Random Hero:', totalRandomHeroPower);
   };
+
 
   return (
     <>
