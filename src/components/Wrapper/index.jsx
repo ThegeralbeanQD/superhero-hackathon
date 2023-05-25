3863146c12cb0e927aa8e4fb20506f0d011b121f
“thegeralbeanQD” <giquigeraldine@outlook.com>
2023-05-25 10:49:46 +0100

    inital commit

---
+++ src/components/Wrapper/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+const styles = ({ isActive }) => ({ color: isActive ? '#ECD444' : '#FFFFFF'});
+
+const PageWrapper = () => {
+    return <>
+        <header>
+            <nav>
+                <NavLink to="/" style={styles}>Home</NavLink>
+                <NavLink to="/characters" style={styles}>Characters</NavLink>
+                <NavLink to="/fight" style={styles}>Fight</NavLink>
+            </nav>
+        </header>
+        <footer>Superhero app</footer>
+        <Outlet />
+    </>
+};
+
+export default PageWrapper;
