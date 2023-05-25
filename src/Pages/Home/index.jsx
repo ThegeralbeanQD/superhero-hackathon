2c6e185c78cc3f698f889af71a01fb5d2ea93f8c
Beckibuzz93 <beckilewis1@hotmail.com>
2023-05-25 11:19:46 +0100

    add API with fake json api, add axios

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import API from '../../components/API'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+      <div><API/></div>
+    </>
   )
 }
 
